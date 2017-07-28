b8117fe2d8b8f34546c8609168945b07da821d25
Jozef Reisinger <jozef.reisinger@gmail.com>
2017-07-28 08:46:25 +0200

    minor change

--- socket_names/www-ping.py
+++ socket_names/www-ping.py
@@ -6,18 +6,26 @@
 import argparse, socket, sys
 
 def connect_to(hostname_or_ip):
+    """General way of getting info about the host/port. Ex. you don't need to
+    specify IP as a protocol or TCP as a transport. All necessary info will be
+    supplied by getaddrinfo()."""
+
+    # Get info needed to connect ...
     try:
         infolist = socket.getaddrinfo(
             hostname_or_ip, 'www', 0, socket.SOCK_STREAM, 0,
             socket.AI_ADDRCONFIG | socket.AI_V4MAPPED | socket.AI_CANONNAME
             )
+    # specific name service error
     except socket.gaierror as e:
         print("Name service failure:", e.args[1])
         sys.exit(1)
 
+    # Connect ...
     info = infolist[0] # per standard recommendation, try the first one
     socket_args = info[0:3]
     address = info[4]
+    # pass the three elements of socket_args as three separate args
     s = socket.socket(*socket_args)
     try:
         s.connect(address)
